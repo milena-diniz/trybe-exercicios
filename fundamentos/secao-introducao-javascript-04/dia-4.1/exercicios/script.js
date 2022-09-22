a36c3e37e87b8d6267861916d795d90a060a044e
Milena Diniz Vieira <miladvieira@outlook.com>
2022-09-22 13:59:03 -0300

    Exercícios 4.1

--- fundamentos/secao-introducao-javascript-04/dia-4.1/exercicios/script.js
+++ fundamentos/secao-introducao-javascript-04/dia-4.1/exercicios/script.js
@@ -6,3 +6,37 @@ console.log(a - b)
 console.log(a * b)
 console.log(a / b)
 console.log(a % b)
+
+let numero1 = 12
+
+let numero2 = 5
+
+if (numero1 > numero2) {
+  console.log('numero1 é maior')
+} else if (numero1 < numero2) {
+  console.log('numero2 é maior')
+} else {
+  console.log('numero1 e numero2 são iguais')
+}
+
+const a = 3
+const b = 8
+const c = 10
+
+if (a > b && a > c) {
+  console.log('a a é maior')
+} else if (b > a && b > c) {
+  console.log('A b é maior')
+} else {
+  console.log('a c é maior')
+}
+
+const number = 4
+
+if (number > 0) {
+  console.log('positive')
+} else if (number < 0) {
+  console.log('negative')
+} else {
+  console.log('zero')
+}
