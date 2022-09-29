016eac41c922c5b0bda62f43260eb6b3270ec19a
Milena Diniz Vieira <miladvieira@outlook.com>
2022-09-29 20:08:49 -0300

    Exercicio Administrador de tempo

--- fundamentos/secao-05/dia-5.1/exercicio2/script.js
+++ fundamentos/secao-05/dia-5.1/exercicio2/script.js
@@ -1,2 +1,24 @@
 const header = document.getElementById('header-container')
-header.style.background = 'red'
+header.style.backgroundColor = 'rgb(0, 176, 105)'
+
+// const container = document.getElementById('container')
+// container.style.backgroundColor = ''
+
+const emergencyTasks = document.querySelector('.emergency-tasks')
+emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)'
+
+const tituloLadoEsquerdo = document.querySelectorAll('.emergency-tasks h3')
+for (let i = 0; i < tituloLadoEsquerdo.length; i += 1) {
+  tituloLadoEsquerdo[i].style.backgroundColor = 'rgb(165, 0, 243)'
+}
+
+const noEmergence = document.querySelector('.no-emergency-tasks')
+noEmergence.style.backgroundColor = 'rgb(249, 219, 94)'
+
+const tituloLadoDireito = document.querySelectorAll('.no-emergency-tasks h3')
+for (let i = 0; i < tituloLadoDireito.length; i += 1) {
+  tituloLadoDireito[i].style.backgroundColor = 'rgb(35, 37, 37)'
+}
+
+const footer = document.getElementById('footer-container')
+footer.style.backgroundColor = 'rgb(0, 53, 51)'
