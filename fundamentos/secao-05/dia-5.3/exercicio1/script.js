dd5dcd5e1efb4d38331d52fa3343a13a2975a04a
Milena Diniz Vieira <miladvieira@outlook.com>
2022-10-03 13:33:07 -0300

    exercicio 5.3

--- fundamentos/secao-05/dia-5.3/exercicio1/script.js
+++ fundamentos/secao-05/dia-5.3/exercicio1/script.js
@@ -9,11 +9,11 @@ function createDaysOfTheWeek() {
     'Sexta',
     'Sábado'
   ]
-  const weekDaysList = document.querySelector('.week-days') // Lista de dias da semana
+  let weekDaysList = document.querySelector('.week-days') // Lista de dias da semana
 
   for (let index = 0; index < weekDays.length; index += 1) {
-    const days = weekDays[index]
-    const dayListItem = document.createElement('li')
+    let days = weekDays[index]
+    let dayListItem = document.createElement('li')
     dayListItem.innerHTML = days
 
     weekDaysList.appendChild(dayListItem)
@@ -21,7 +21,7 @@ function createDaysOfTheWeek() {
 }
 
 createDaysOfTheWeek()
-
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
 const decemberDaysList = [
   29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
@@ -29,11 +29,12 @@ const decemberDaysList = [
 
 function createDayCalendar() {
   // cria dias do mês
-  const primeiraSemana = document.querySelector('#days')
+  let primeiraSemana = document.querySelector('#days')
 
   for (let i = 0; i < decemberDaysList.length; i += 1) {
-    const valorDoIndice = decemberDaysList[i]
-    const creatList = document.createElement('li')
+    let valorDoIndice = decemberDaysList[i]
+    let creatList = document.createElement('li')
+
     creatList.innerHTML = valorDoIndice
 
     if (valorDoIndice === 24 || valorDoIndice === 31) {
@@ -44,7 +45,7 @@ function createDayCalendar() {
       valorDoIndice === 11 ||
       valorDoIndice === 18
     ) {
-      creatList.className = 'day holiday'
+      creatList.className = 'day friday'
       primeiraSemana.appendChild(creatList)
     } else if (valorDoIndice === 25) {
       creatList.className = 'day holiday friday'
@@ -57,9 +58,10 @@ function createDayCalendar() {
 }
 createDayCalendar()
 
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
 function button(string) {
-  const pai = document.querySelector('.buttons-container')
-  const button = document.createElement('button')
+  let pai = document.querySelector('.buttons-container')
+  let button = document.createElement('button')
 
   button.innerHTML = string
   button.id = 'btn-holiday'
@@ -67,18 +69,75 @@ function button(string) {
 }
 button('Feriados')
 
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
 function mudaACorDeFundo() {
-  const button = document.querySelector('#btn-holiday')
-  const allButtons = document.querySelectorAll('.holiday')
+  let button = document.querySelector('#btn-holiday')
+  let allButtons = document.querySelectorAll('.holiday')
+  let corDeFundo = 'white'
 
   button.addEventListener('click', function () {
     for (let i = 0; i < allButtons.length; i += 1) {
-      if (allButtons[i].style.backgroundColor === white) {
+      if (allButtons[i].style.backgroundColor === corDeFundo) {
         allButtons[i].style.backgroundColor = 'rgb(238,238,238'
       } else {
-        allButtons[i].style.backgroundColor = 'white'
+        allButtons[i].style.backgroundColor = corDeFundo
       }
     }
   })
 }
 mudaACorDeFundo()
+
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+function buttonSexta(string) {
+  let container = document.querySelector('.buttons-container')
+  let buttonSextou = document.createElement('button')
+
+  buttonSextou.innerHTML = string
+  buttonSextou.id = 'btn-friday'
+  container.appendChild(buttonSextou)
+}
+buttonSexta('Sexta-feira')
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+function sextou(friday) {
+  let button = document.querySelector('#btn-friday')
+  let allButtons = document.getElementsByClassName('friday')
+  let sextaFeira = 'SEXTOU'
+
+  button.addEventListener('click', function () {
+    for (let i = 0; i < allButtons.length; i += 1) {
+      if (allButtons[i].innerHTML !== sextaFeira) {
+        allButtons[i].innerHTML = sextaFeira
+      } else {
+        allButtons[i].innerHTML = friday[i]
+      }
+    }
+  })
+}
+sextou([4, 11, 18, 25])
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
+function eventTarget() {
+  let days = document.querySelectorAll('.day')
+
+  console.log(days)
+
+  for (let i in days) {
+    let day = days[i]
+    if (!day) continue
+
+    day.addEventListener('mouseover', function (e) {
+      e.target.style.fontSize = '30px'
+      e.target.style.fontWeight = '600'
+    })
+  }
+}
+eventTarget()
+function eventTarget2() {
+  let days = document.querySelectorAll('.day')
+  days.addEventListener('mouseout', function (e) {
+    e.target.style.fontSize = '20px'
+    e.target.style.fontWeight = '600'
+  })
+}
+
+eventTarget2()
+/*---------------------------------------------------------------------------------------------------------------------------------------*/
