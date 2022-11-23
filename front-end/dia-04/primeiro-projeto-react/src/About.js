35f3b78faaa947447f1b273222d5259e021542f9
Milena Diniz Vieira <miladvieira@outlook.com>
2022-11-23 15:49:09 -0300

    Organizando

---
+++ front-end/dia-04/primeiro-projeto-react/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class SobreMim extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Milena Diniz Vieira:</h1>
+        <p>
+          <ol>
+            <li>Introdução - Front-end</li>
+            <li>Introdução - React</li>
+            <li>'Hello, world!' no React!</li>
+            <li>Componentes React</li>
+          </ol>
+        </p>
+      </div>
+    )
+  }
+} 
+
+export default SobreMim;
---
+++ front-end/dia-04/primeiro-projeto-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SobreMim from './About';
+
+class App extends React.Component {
+  render() {
+    return (
+      <SobreMim />
+    );
+  }
+}
+
+export default App;
